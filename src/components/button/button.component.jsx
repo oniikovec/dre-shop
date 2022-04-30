e779a2bfc5e47a7a8ec074eddb25acbd3ea05db3
Oni <57707799+oniikovec@users.noreply.github.com>
2022-04-30 11:00:13 +0200

    finished payment and created payment button

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,6 @@
 // default, inverted and google sign in button
 
-import { BaseButton, GoogleSignInButton, InvertedButton } from "./button.styles";
+import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from "./button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
   base: 'base',
@@ -16,13 +16,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
   }[buttonType]
 )
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
 
   const CustomButton = getButton(buttonType)
 
   return (
-    <CustomButton {...otherProps}>
-      {children}
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
     </CustomButton>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js'
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import App from './App';
 import { store, persistor } from './store/store';
 
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-root.render(
+render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  rootElement
+);
